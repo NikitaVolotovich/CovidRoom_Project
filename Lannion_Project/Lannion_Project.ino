0e67606da4fb30177c3ec6c901ba33f5cc33b2b2
Mikita Valatovich <mr.volotovich@gmail.com>
2021-07-08 13:13:28 +0200

    Ver. 0.9

--- Lannion_Project/Lannion_Project.ino
+++ Lannion_Project/Lannion_Project.ino
@@ -6,12 +6,13 @@
 #include <Wire.h>
 #include "DFRobot_RGBLCD.h"
 #include <Servo.h>
+#include <SoftwareSerial.h>
 
-#define OUTSIDE_LED_PIN 2
+#define OUTSIDE_LED_PIN 8
 #define INSIDE_LED_PIN 3
 #define BLUETOOTH_RX_PIN 4
 #define BLUETOOTH_TX_PIN 5
-#define EMERGENCY_BUTTON 8
+#define EMERGENCY_BUTTON 2
 #define MOTOR_PIN 6
 
 #define ULTRASONIC_SENSOR_OUTSIDE_TRIG 9
@@ -21,46 +22,49 @@
 
 #define AQ_SENSOR A2
 
-#define START_MODE 0
-#define NORMAL_MODE 1
-#define BAD_AIR_MODE 2
-#define BAD_COUNT_MODE 3
-#define BAD_COUNT_AND_AIR_MODE 4
-#define EMERGENCY_MODE 5
+#define NORMAL_MODE 0
+#define BAD_AIR_MODE 1
+#define BAD_COUNT_MODE 2
+#define BAD_COUNT_AND_AIR_MODE 3
+#define EMERGENCY_MODE 4
 
 #define NORMAL_LED_MODE 0
 #define ALARM_LED_MODE 1
 
 #define NORMAL_AIR_QUALITY 300
-#define MAXIMUM_AIR_QUALITY 600
+#define MAXIMUM_AIR_QUALITY 450
 #define MAXIMUM_PEOPLE 10
 #define MAXIMUM_LED_BRIGHT 128
-#define SENSOR_MISTAKE_PERCENT 0.05
+#define SENSOR_MISTAKE_PERCENT 0.20
+#define DELAY_AFTER_DETECTING 100
+#define BLINKING_PERIOD 10
 
-#define DOOR_OPEN_STATE 110
-#define DOOR_CLOSE_STATE 700
+#define DOOR_OPEN_STATE 500
+#define DOOR_CLOSE_STATE 0
 #define TIME_FOR_EXIT 1000
 
 int mode = 0, LEDMode = 0;
 int airQuality = 0;
 
-int delayForDetecting = 201;
+int delayForDetecting = 25;
 bool doDetecting = true;
 float ultrasonicOutside = 0, ultrasonicInside = 0;
 int ultrasonicSystematicErrorOutside, ultrasonicSystematicErrorInside;
 int ultrasonicOutsideNormalDistance  = 0, ultrasonicInsideNormalDistance = 0;
-int peopleCounter = 0;
+int peopleCounter = 0, previousPeopleCounter = 0;
 
 bool buttonIsPressed = false, doorIsOpen = false;
 float distance, sensity;
-int timeCounter = 0, timeForEmergencyExit = 0;
+int timeCounter = 0, timeForEmergencyExit = 0, blinkingLEDTimer = 0;
 
-bool somethingWasDetected = false;
+bool somethingWasDetected = false, blinkingNow = false;
 
-int LCDRed,LCDGreen, LCDBlue;
+int peopleCounterLEDRed = 0, peopleCounterLEDGreen = 0, peopleCounterLEDBlue = 0;
+int AirQualityLEDRed = 0, AirQualityLEDGreen = 0, AirQualityLEDBlue = 0;
+int LCDRed = 0,LCDGreen = 0, LCDBlue = 0;
 int LCDTime = 0;
-///////
 
+SoftwareSerial BTSerial(BLUETOOTH_RX_PIN, BLUETOOTH_TX_PIN);
 Adafruit_NeoPixel outsideLED = Adafruit_NeoPixel(1, OUTSIDE_LED_PIN, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel insideLED = Adafruit_NeoPixel(1, INSIDE_LED_PIN, NEO_GRB + NEO_KHZ800);
 Servo motor;
@@ -70,14 +74,14 @@ void setup() {
   lcd.init();
 
   motor.attach(MOTOR_PIN);
-  motor.writeMicroseconds(800); 
+  motor.write(0); 
   
   pinMode(ULTRASONIC_SENSOR_OUTSIDE_TRIG, OUTPUT);
   pinMode(ULTRASONIC_SENSOR_OUTSIDE_ECHO, INPUT); 
   pinMode(ULTRASONIC_SENSOR_INSIDE_TRIG, OUTPUT); 
   pinMode(ULTRASONIC_SENSOR_INSIDE_ECHO, INPUT); 
-  pinMode(EMERGENCY_BUTTON, INPUT);
-  attachInterrupt(1, buttonInterrupt, FALLING);
+  pinMode(EMERGENCY_BUTTON, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(EMERGENCY_BUTTON), buttonInterrupt, FALLING);
   
   ultrasonicValueUpdate();
   
@@ -94,6 +98,7 @@ void setup() {
   insideLED.begin();
 
   Serial.begin(115200);
+  BTSerial.begin(115200);
   
   Timer2.setPeriod(100000);
   Timer2.enableISR(CHANNEL_A);
@@ -107,6 +112,8 @@ void setup() {
   Serial.println(ultrasonicSystematicErrorOutside);
   Serial.print(" error inside cm: ");
   Serial.println(ultrasonicSystematicErrorInside);
+
+  BTSerial.println("Initialization was successful");
 }
 
 void loop() {
@@ -115,39 +122,24 @@ void loop() {
   updatePeripherals();
   modeSelector();
   modeExecutor();
-//  outputEverything();
+  outputEverything();
 }
 
 void controlInsideLED() {
-  if(airQuality != 0 && airQuality < MAXIMUM_AIR_QUALITY) {
-    float indexAir = (float) MAXIMUM_AIR_QUALITY / airQuality;
-    float redColorBright_In = MAXIMUM_LED_BRIGHT / indexAir, greenColorBright_In = MAXIMUM_LED_BRIGHT - redColorBright_In;
-    insideLED.clear();   
-    insideLED.setPixelColor(0, insideLED.Color(((int) redColorBright_In), ((int) greenColorBright_In), 0));
-    insideLED.show();
-  }
+  insideLED.clear();   
+  insideLED.setPixelColor(0, insideLED.Color(AirQualityLEDRed, AirQualityLEDGreen, AirQualityLEDBlue));
+  insideLED.show();
 }
 
 void controlOutsideLED() {
-  if(peopleCounter == 0) {
-    outsideLED.clear();  
-    outsideLED.setPixelColor(0, outsideLED.Color(0, MAXIMUM_LED_BRIGHT, 0));
-    outsideLED.show();
-  } else if(peopleCounter != 0 && peopleCounter <= MAXIMUM_PEOPLE) {
-    float  indexPeople = (float) MAXIMUM_PEOPLE / peopleCounter;
-    int redColorBright_Out = MAXIMUM_LED_BRIGHT / indexPeople;
-    int greenColorBright_Out = MAXIMUM_LED_BRIGHT - redColorBright_Out;
-    outsideLED.clear();  
-    outsideLED.setPixelColor(0, outsideLED.Color(((int) redColorBright_Out), ((int) greenColorBright_Out), 0));
-    outsideLED.show();
-  } 
+  outsideLED.clear();  
+  outsideLED.setPixelColor(0, outsideLED.Color(peopleCounterLEDRed, peopleCounterLEDGreen, peopleCounterLEDBlue));
+  outsideLED.show();
 }
 
 
 void modeSelector() {
-  if(airQuality < NORMAL_AIR_QUALITY && peopleCounter == 0 && !buttonIsPressed){
-    mode = START_MODE;
-  } else if(airQuality < NORMAL_AIR_QUALITY && peopleCounter <= MAXIMUM_PEOPLE && peopleCounter != 0 && !buttonIsPressed){
+  if(airQuality < NORMAL_AIR_QUALITY && peopleCounter <= MAXIMUM_PEOPLE && !buttonIsPressed){
     mode = NORMAL_MODE;
   } else if (airQuality > NORMAL_AIR_QUALITY && peopleCounter <= MAXIMUM_PEOPLE && !buttonIsPressed){
     mode = BAD_AIR_MODE;
@@ -163,38 +155,40 @@ void modeSelector() {
 
 void modeExecutor() {
   switch (mode) {
-    case START_MODE:
-    
-      openDoor();
-      break;
     case NORMAL_MODE:
-    
-      openDoor();
-      break;
+    LEDMode = NORMAL_LED_MODE;
+    openDoor();
+    break;
+      
     case BAD_AIR_MODE:
-    
-      closeDoor();
-      break;
+    LEDMode = ALARM_LED_MODE;
+    closeDoor();
+    break;
+      
     case BAD_COUNT_MODE:
-
-      closeDoor();
-      break;
+    LEDMode = ALARM_LED_MODE;
+    closeDoor();
+    break;
+      
     case BAD_COUNT_AND_AIR_MODE:
-    
-      break;
+    LEDMode = ALARM_LED_MODE;
+    closeDoor();
+    break;
+      
     case EMERGENCY_MODE:
-    
-      openDoor();
-      break;
+    LEDMode = ALARM_LED_MODE;
+    openDoor();
+    break;
   } 
 }
 
-
-
 void updatePeripherals() {
   showOnLCD();
+  colorRegulator();
   controlInsideLED();
   controlOutsideLED();
+  outputEverything();
+  outputEverythingToDashboard();
   airQuality = analogRead(AQ_SENSOR);
 
   if(timeForEmergencyExit > TIME_FOR_EXIT){
@@ -233,7 +227,9 @@ void detectPerson() {
     }
     if(ultrasonicInside < ultrasonicInsideNormalDistance - ultrasonicSystematicErrorInside && somethingWasDetected == false){
       Serial.println("INSIDE--");
-      peopleCounter--;
+      if(peopleCounter != 0){
+        peopleCounter--;
+      }
       somethingWasDetected = true;
     }
     if(ultrasonicInside + ultrasonicSystematicErrorInside > ultrasonicInsideNormalDistance &&
@@ -265,53 +261,117 @@ void closeDoor() {
 }
 
 void showOnLCD(){
-  if(LEDMode == NORMAL_LED_MODE){
-    LCDRed = (abs(sin(3.14*LCDTime/180)))*255;       
-    LCDGreen = (abs(sin(3.14*(LCDTime+60)/180)))*255;
-    LCDBlue = (abs(sin(3.14*(LCDTime+120)/180)))*255;
-    LCDTime += 3;
-  } else if(LEDMode == ALARM_LED_MODE){
-    LCDRed = 255;      
-    LCDGreen = 0;
-    LCDBlue = 0;
+  if(peopleCounter != previousPeopleCounter){
+    lcd.clear();
   }
-//  lcd.clear();
+  previousPeopleCounter = peopleCounter;
   lcd.setRGB(LCDRed, LCDGreen, LCDBlue);    
   lcd.setCursor(0,0);
-  lcd.print("Counter: ");
-  lcd.print(peopleCounter);
+  
+  lcd.print("Counter: " + String(peopleCounter));
   lcd.setCursor(0,1);
-  lcd.print("AirQuality: ");
-  lcd.print(airQuality);
+  lcd.print("Air: " + String(airQuality));
+}
+
+void colorRegulator(){
+    switch(LEDMode){
+      case NORMAL_LED_MODE:
+        AirQualityLEDRed = MAXIMUM_LED_BRIGHT / ((float) MAXIMUM_AIR_QUALITY / airQuality);
+        AirQualityLEDGreen = MAXIMUM_LED_BRIGHT - AirQualityLEDRed;
+        if(peopleCounter == 0){
+          peopleCounterLEDRed = 0;
+          peopleCounterLEDGreen = MAXIMUM_LED_BRIGHT;
+        } else {
+          peopleCounterLEDRed = MAXIMUM_LED_BRIGHT / ((float) MAXIMUM_PEOPLE / peopleCounter);
+          peopleCounterLEDGreen = MAXIMUM_LED_BRIGHT - peopleCounterLEDRed;
+        }
+        LCDRed = (abs(sin(3.14*LCDTime/180)))*255;       
+        LCDGreen = (abs(sin(3.14*(LCDTime+60)/180)))*255;
+        LCDBlue = (abs(sin(3.14*(LCDTime+120)/180)))*255;
+        LCDTime += 3;
+        break;
+      case ALARM_LED_MODE:
+        if(blinkingNow){
+          AirQualityLEDRed = MAXIMUM_LED_BRIGHT;
+          AirQualityLEDGreen = 0;
+          peopleCounterLEDRed = MAXIMUM_LED_BRIGHT;
+          peopleCounterLEDGreen = 0;
+          LCDRed = 255;      
+          LCDGreen = 0;
+          LCDBlue = 0;
+        } else if(!blinkingNow) {
+          AirQualityLEDRed = 0;
+          AirQualityLEDGreen = 0;
+          peopleCounterLEDRed = 0;
+          peopleCounterLEDGreen = 0;
+          LCDRed = 0;      
+          LCDGreen = 0;
+          LCDBlue = 0;
+        }
+        break;
+    }
 }
 
 void outputEverything() {
-  Serial.print("Current mode: ");
-  Serial.println(mode);
-  Serial.print("Ultrasonic outside cm: ");
-  Serial.println(ultrasonicOutside);
-  Serial.print("Ultrasonic inside cm: ");
-  Serial.println(ultrasonicInside);
-  Serial.print("Button is pressed: ");
-  Serial.println(buttonIsPressed);
-  Serial.print("Air quality: ");
-  Serial.println(airQuality);
-  Serial.print("People counter: ");
-  Serial.println(peopleCounter);
-  Serial.print("Time counter: ");
-  Serial.println(timeCounter);
+  Serial.print("Current mode: "+ String(mode) + "\n");
+  Serial.print("Ultrasonic outside cm: " + String(ultrasonicOutside) + "\n");
+  Serial.print("Ultrasonic inside cm: " + String(ultrasonicInside) + "\n");
+  Serial.print("Button is pressed: " + String(buttonIsPressed) + "\n");
+  Serial.print("Air quality: " + String(airQuality) + "\n");
+  Serial.print("People counter: " + String(peopleCounter) + "\n");
+  Serial.print("Time counter: " + String(timeCounter) + "\n");
+}
+
+void outputEverythingToDashboard() {
+  BTSerial.print("M" + String(mode) + "*");
+  BTSerial.print("O" + String(ultrasonicOutside) + "*");
+  BTSerial.print("I" + String(ultrasonicInside) + "*");
+  BTSerial.print("B" + String(buttonIsPressed) + "*");
+  BTSerial.print("A" + String(airQuality) + "*");
+  BTSerial.print("P" + String(peopleCounter) + "*");
+  BTSerial.print("T" + String(timeCounter) + "*");
+  BTSerial.print("CR" + String(LCDRed) + "G" + String(LCDGreen) + "B" + String(LCDBlue) + "*");
+  BTSerial.print("QR" + String(AirQualityLEDRed) + "G" + String(AirQualityLEDGreen*2) + "B" + String(AirQualityLEDBlue) + "*");
+  BTSerial.print("KR" + String(peopleCounterLEDRed) + "G" + String(peopleCounterLEDGreen) + "B" + String(peopleCounterLEDBlue) + "*");
+  BTSerial.print("D" + String(ultrasonicOutsideNormalDistance) + "*");
+  BTSerial.print("E" + String(ultrasonicInsideNormalDistance) + "*");
+  BTSerial.print("Y" + String(ultrasonicSystematicErrorOutside) + "*");
+  BTSerial.print("Z" + String(ultrasonicSystematicErrorInside) + "*");
+  
+  switch(mode){
+    case NORMAL_MODE: 
+      BTSerial.print("S JUST CHILL*");
+      break;
+    case BAD_AIR_MODE:
+      BTSerial.print("S OPEN A WINDOW*");
+      break;
+    case BAD_COUNT_MODE:
+      BTSerial.print("S POLICE WILL COMING SOON*");
+      break;
+    case BAD_COUNT_AND_AIR_MODE:
+      BTSerial.print("S EVERYTHING IS BAD*");
+      break;
+    case EMERGENCY_MODE:
+      BTSerial.print("S GO AWAY*");
+      break;
+  }
 }
 
 ISR(TIMER2_A) {
-
   if(!doDetecting){
     delayForDetecting++;
-    if(delayForDetecting > 200){
+    if(delayForDetecting > DELAY_AFTER_DETECTING){
       doDetecting = true;
     }
   }
   if(timeForEmergencyExit != 0){
     timeForEmergencyExit++;
+  }
+  if(LEDMode == ALARM_LED_MODE){
+    blinkingLEDTimer++;
+    if(blinkingLEDTimer > BLINKING_PERIOD){
+      blinkingNow = !blinkingNow;
+    }
   }
   timeCounter++;
 }
